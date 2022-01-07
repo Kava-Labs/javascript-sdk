e0ec217ff69cae4ffb516d942addc53006e153d5
denalimarsh <denalimarsh@gmail.com>
2022-01-07 14:14:11 +0100

    env: include local

--- example/static/env.ts
+++ example/static/env.ts
@@ -20,11 +20,19 @@ const KavaEndpoints = {
 };
 
 const KavaDeputy = {
+  Local: {
+    Address: '',
+    Mnemonic: '',
+  },
   Testnet: 'kava1tfvn5t8qwngqd2q427za2mel48pcus3z9u73fl',
   Mainnet: 'kava1r4v2zdhdalfj2ydazallqvrus9fkphmglhn6u6',
 };
 
 const BinanceAccount = {
+  Local: {
+    Address: '',
+    Mnemonic: '',
+  },
   Testnet: {
     Address: '',
     Mnemonic: '',
@@ -36,11 +44,13 @@ const BinanceAccount = {
 };
 
 const BinanceEndpoints = {
+  Local: 'http://localhost:8080',
   Testnet: 'https://testnet-dex.binance.org',
   Mainnet: 'https://dex.binance.org/',
 };
 
 const BinanceDeputy = {
+  Local: '',
   Testnet: 'tbnb1et8vmd0dgvswjnyaf73ez8ye0jehc8a7t7fljv',
   Mainnet: 'bnb1jh7uv2rm6339yue8k4mj9406k3509kr4wt5nxn',
 };
