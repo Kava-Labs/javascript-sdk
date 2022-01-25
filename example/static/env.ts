f6f18704efe417b6826fb8b46befa2a25b362cca
Thomas Constantine Moore <talk@thomas.tech>
2022-01-25 14:38:47 -0500

    Support .env configuration

--- example/static/env.ts
+++ example/static/env.ts
@@ -1,7 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 const KavaAccount = {
   Local: {
-    Address: '',
-    Mnemonic: '',
+    Address: process.env.KAVA_ADDRESS,
+    Mnemonic: process.env.KAVA_MNEMONIC,
   },
   Testnet: {
     Address: '',
@@ -20,11 +23,19 @@ const KavaEndpoints = {
 };
 
 const KavaDeputy = {
+  Local: {
+    Address: process.env.DEPUTY_ADDRESS,
+    Mnemonic: process.env.DEPUTY_MNEMONIC,
+  },
   Testnet: 'kava1tfvn5t8qwngqd2q427za2mel48pcus3z9u73fl',
   Mainnet: 'kava1r4v2zdhdalfj2ydazallqvrus9fkphmglhn6u6',
 };
 
 const BinanceAccount = {
+  Local: {
+    Address: process.env.BINANCE_ADDRESS,
+    Mnemonic: process.env.BINANCE_MNEMONIC,
+  },
   Testnet: {
     Address: '',
     Mnemonic: '',
@@ -36,11 +47,13 @@ const BinanceAccount = {
 };
 
 const BinanceEndpoints = {
+  Local: process.env.BINANCE_LOCAL_ENDPOINT,
   Testnet: 'https://testnet-dex.binance.org',
   Mainnet: 'https://dex.binance.org/',
 };
 
 const BinanceDeputy = {
+  Local: process.env.BINANCE_DEPUTY_ADDRESS,
   Testnet: 'tbnb1et8vmd0dgvswjnyaf73ez8ye0jehc8a7t7fljv',
   Mainnet: 'bnb1jh7uv2rm6339yue8k4mj9406k3509kr4wt5nxn',
 };
