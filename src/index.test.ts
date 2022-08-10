8cbf345604e6fe48329a63660da18369d13ea6ca
samcode206 <hadi.sameem.dev@gmail.com>
2022-08-10 12:53:16 -0500

    add Address Conversions

--- src/index.test.ts
+++ src/index.test.ts
@@ -17,4 +17,16 @@ describe('SDK exports', () => {
     expect(msg).toBeDefined();
     expect(crypto).toBeDefined();
   });
+
+  it('derives the correct kava address from ox address', () => {
+    expect(
+      utils.kavaToEthAddress('kava1vlpsrmdyuywvaqrv7rx6xga224sqfwz3fyfhwq')
+    ).toEqual('0x67C301eDA4E11Cce806Cf0CDa323aA556004b851');
+  });
+
+  it('derives the correct 0x address from a kava address', () => {
+    expect(
+      utils.ethToKavaAddress('0x7Bbf300890857b8c241b219C6a489431669b3aFA')
+    ).toEqual('kava10wlnqzyss4accfqmyxwx5jy5x9nfkwh6qm7n4t');
+  });
 });
