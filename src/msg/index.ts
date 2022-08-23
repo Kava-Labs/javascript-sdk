7e0c094cc378874f72bf04bc356602367e812617
Paul Downing <downing034@gmail.com>
2022-08-23 10:00:01 -0600

    add earn msg formatting

--- src/msg/index.ts
+++ src/msg/index.ts
@@ -2,10 +2,12 @@ import { cosmos } from './cosmos';
 import { kava } from './kava';
 import { hard } from './hard';
 import { swap } from './swap';
+import { earn } from './earn';
 
 export const msg = {
   cosmos,
   kava,
   hard,
   swap,
+  earn,
 };
